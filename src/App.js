bac7bd6533472a40f56742f51c9544899fb9cb79
Rami <138556019+rami242424@users.noreply.github.com>
2023-11-22 12:11:22 +0900

    아무 사각형을 클릭하여 X를 다시 추가할 수 있음

--- src/App.js
+++ src/App.js
@@ -9,43 +9,30 @@ function Square({ value, onSquareClick }) {
 }
 
 export default function Board() {
-  // 9개의 null배열을 기본값으로 하는 squares라는 state 변수를 선언
   const [squares, setSquares] = useState(Array(9).fill(null));
 
-  function handleClick(){
+  function handleClick(i) {
     const nextSquares = squares.slice();
-    nextSquares[0] = "x";
+    nextSquares[i] = "x";
     setSquares(nextSquares);
   }
-  // handleClick함수는 자바스크립트의 slice()배열 메서드를 사용하여,
-  // squares 배열의 사본(nextSquares)을 생성합니다. 
-  // 그런 다음 handleClick 함수는 nextSquares 배열의 첫 번째 사각형에 x를 추가하여 업데이트 합니다.
-  // **setSquares 함수를 호출**하면 React는 컴포넌트의 state가 변경되었음을 알 수 있습니다.
-  // 그러면 squares의 state를 사용하는 컴포넌트와 그 하위 컴포넌트가 다시 렌더링 됩니다.
-  // 즉, 위 handleClick 함수의 내용은 
-  // 1. slice 배열 메서드를 사용하여 squares배열의 사본 배열 nextSquares를 정의(생성)한다.
-  // 2. nextSquares 배열의 0번째 인덱스에 x값을 정의한다.
-  // 3. setSquares 함수를 호출한다.
-  // -> React가 컴포넌트의 state가 변경됨을 전달함
-  // -> squares의 state를 사용하는 컴포넌트, 하위 컴포넌트들이 다시 렌더링된다.
-
-
   return (
     <>
       <div className="board-row">
-        <Square value={squares[0]} onSquareClick={handleClick} />
-        <Square value={squares[1]} />
-        <Square value={squares[2]} />
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+        {/* 사각형이 클릭되면, => 뒤에 코드가 실행되어 handleClick(0)을 호출합니다. */}
+        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
       </div>
       <div className="board-row">
-        <Square value={squares[3]} />
-        <Square value={squares[4]} />
-        <Square value={squares[5]} />
+        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
       </div>
       <div className="board-row">
-        <Square value={squares[6]} />
-        <Square value={squares[7]} />
-        <Square value={squares[8]} />
+        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
       </div>
     </>
   );
